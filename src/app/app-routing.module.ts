1bf66b7d152599928d90b1ac6c45dd16502cceaa
Ayanda Matiringe <acm@Ayandas-MacBook-Pro.local>
2018-11-22 10:09:21 +0100

    adds redirect to invalid routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [{path: 'home', component: HomeComponent },
-  {path: 'about-me', component: AboutComponent  }
-
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
